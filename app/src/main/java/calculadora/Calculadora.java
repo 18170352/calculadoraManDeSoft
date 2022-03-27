e440126c2e264646834f5ce95bcc66744351e916
18170352 <102200357+18170352@users.noreply.github.com>
2022-03-27 16:48:53 -0700

    Initial commit

---
+++ app/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Calculadora {
+
+    public double suma(double x, double y) {
+        return x + y;
+    }
+    
+    public double multiplica(double x, double y) {
+        return x * y;
+    }
+}
